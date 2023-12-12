15447497f4b8497e2c8d88dfdb6a8ed95df202d6
Andrew Sithole <andrewsithole7@gmail.com>
2023-12-12 17:05:50 +0200

    removed unused imports

--- lib/src/date_picker/app_calender.dart
+++ lib/src/date_picker/app_calender.dart
@@ -1,5 +1,3 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
 import 'package:flutter_ui_components/flutter_ui_components.dart';
