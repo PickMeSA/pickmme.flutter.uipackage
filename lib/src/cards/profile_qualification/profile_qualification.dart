840a854979d402000547330619143e0555873057
Authority10240 <48833225+Authority10240@users.noreply.github.com>
2024-02-01 12:15:04 +0200

    fixed work experience widget

--- lib/src/cards/profile_qualification/profile_qualification.dart
+++ lib/src/cards/profile_qualification/profile_qualification.dart
@@ -95,7 +95,7 @@ class AppProfileQualification extends StatelessWidget{
                     if(qualification.dateStarted != null)
                     Text(
                     "${qualification.dateStarted!.toMonthYearString()} - "
-                    "${qualification.dateEnded!= null? qualification.dateEnded:toPresent} ",
+                    "${qualification.dateEnded ?? toPresent} ",
                     style: theme.textTheme.bodySmall!.copyWith(color: neutrals500Color),
                     )
                   ],
