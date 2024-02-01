77313e9a7e61e91a232822172da295f904538972
Andrew Sithole <andrewsithole7@gmail.com>
2024-02-01 15:22:59 +0200

    Made job start date nullable

--- lib/src/view_toggles/tabs.dart
+++ lib/src/view_toggles/tabs.dart
@@ -7,12 +7,17 @@ class AppTabBar extends StatelessWidget {
   final double? viewHeight;
   final int? initialIndex;
   final Function(int index) onTap;
+  final bool isScrollable;
+  final TabBarIndicatorSize indicatorSize;
+
   const AppTabBar({
     super.key,
     required this.tabs,
     required this.views,
     this.viewHeight,
     this.initialIndex,
+    this.isScrollable = false,
+    this.indicatorSize = TabBarIndicatorSize.tab,
     required this.onTap,
   }): assert(tabs.length!=0 && tabs.length==views.length);
 
@@ -26,9 +31,11 @@ class AppTabBar extends StatelessWidget {
           Material(
             color: neutrals100Color,
             child: TabBar(
+              indicatorColor: primaryColor,
+              isScrollable: isScrollable,
               padding: const EdgeInsets.all(4),
               dividerColor: neutrals100Color,
-              indicatorSize: TabBarIndicatorSize.tab,
+              indicatorSize: indicatorSize,
               onTap: (index)=> onTap(index),
               tabs: tabs.map((e) => Padding(padding: const EdgeInsets.all(8),child: e)
               ).toList(),
